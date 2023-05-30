6ec0f52683d5e3850b0ac7e14b73ddd81e330cdf
Vladimer Grigalashvili <lado.grigalashvili@gmail.com>
2023-05-30 17:14:25 +0400

    added:  `TypeOrmConfigService`.

--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import appConfig from './config/app.config';
 import authConfig from './config/auth.config';
 import databaseConfig from './config/database.config';
 import mqttConfig from './config/mqtt.config';
+
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { DataSource, DataSourceOptions } from 'typeorm';
 
 @Module({
 	imports: [
@@ -13,6 +17,13 @@ import mqttConfig from './config/mqtt.config';
 			ignoreEnvFile: process.env['NODE_ENV'] === 'development' || process.env['NODE_ENV'] === 'test' ? false : true,
 			isGlobal: true,
 			load: [appConfig, authConfig, databaseConfig, mqttConfig],
+		}),
+
+		TypeOrmModule.forRootAsync({
+			useClass: TypeOrmConfigService,
+			dataSourceFactory: async (options: DataSourceOptions) => {
+				return new DataSource(options).initialize();
+			},
 		}),
 	],
 	providers: [],
