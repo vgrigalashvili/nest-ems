49f038cc22c11ac110bf7818d651f1ea015e9de3
Vladimer Grigalashvili <lado.grigalashvili@gmail.com>
2023-06-13 13:41:22 +0400

    fixed: role-base access [`guard`]

--- src/modules/role/guard/role.guard.ts
+++ src/modules/role/guard/role.guard.ts
@@ -1,17 +1,22 @@
 import { Reflector } from '@nestjs/core';
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 
+import { UserRoleService } from '../../user/service';
+
 @Injectable()
 export class RoleGuard implements CanActivate {
-	constructor(private reflector: Reflector) {}
+	constructor(private reflector: Reflector, private userRoleService: UserRoleService) {}
 
-	canActivate(context: ExecutionContext): boolean {
+	async canActivate(context: ExecutionContext): Promise<boolean> {
 		const roles = this.reflector.getAllAndOverride<number[]>('role', [context.getClass(), context.getHandler()]);
+
 		if (!roles.length) {
 			return true;
 		}
-		const request = context.switchToHttp().getRequest();
 
-		return roles.includes(request.user?.role?.id);
+		const request = context.switchToHttp().getRequest();
+		const userRoles = await this.userRoleService.getUserRoles({ id: request.user.id });
+
+		return roles.some((role) => userRoles.includes(role));
 	}
 }
